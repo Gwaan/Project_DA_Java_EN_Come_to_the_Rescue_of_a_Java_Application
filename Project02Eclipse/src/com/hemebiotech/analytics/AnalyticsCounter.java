937b44d96934ceec6d2889ac51bac405185a52e0
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2020-11-08 14:13:33 +0100

    Ajout de la javadoc pour SymptomSorter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,9 +3,9 @@ package com.hemebiotech.analytics;
 import java.util.Map;
 
 /**
- * Programme lisant une source (fichier) de symptômes, comptant chaque
+ * <p>Programme lisant une source (fichier) de symptômes, comptant chaque
  * occurence, les triant par ordre alphabétique puis les enregistrant dans un
- * fichier cible
+ * fichier cible</p>
  * 
  * @author Gwen
  * @version 1.0
